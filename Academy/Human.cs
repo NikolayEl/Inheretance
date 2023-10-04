8619f8f9ba486965b8cbb5341661ad898bc60299
Nikolay Elshin <nikolay19830223@gmail.com>
2023-10-04 20:34:03 +0800

    Add project files.

---
+++ Academy/Human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    internal class Human
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+        public Human(string lastName, string firstName, int age)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Age = age;
+            Console.WriteLine($"HConstructor:\t {this.GetHashCode()}");
+        }
+        ~Human()
+        {
+            Console.WriteLine($"HDesstructor:\t {this.GetHashCode()}");
+        }
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} {Age}";
+        }
+    }
+}
