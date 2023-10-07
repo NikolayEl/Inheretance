ee2a46f071aa86469cac2cb05ca4c4b18d80e19e
Nikolay Elshin <nikolay19830223@gmail.com>
2023-10-07 20:17:34 +0800

    class Shape is done

--- GeometryFigure/Program.cs
+++ GeometryFigure/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace GeometryFigure
 {
@@ -10,6 +12,7 @@ namespace GeometryFigure
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
--- GeometryFigure/Shape.cs
+++ GeometryFigure/Shape.cs
@@ -1,13 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GeometryFigure
 {
+
     internal class Shape
     {
+        // Ограничения до границ экрана
+        const int MinStartX = 10;
+        const int MinStartY = 10;       
+        const int MaxStartX = 500;
+        const int MaxStartY = 600;
+        ////////////////////////////////
+        public int StartX {  get; set; }
+        public int StartY { get; set; }
+        public int LineWidth { get; set; }
+        public Color color { get; set; }
+
+        static Color ColorSelection(string select)
+        {
+            switch (select)
+            {
+                case "red":
+                    return Color.Red;
+                    break;
+                case "green":
+                    return Color.Green;
+                    break;
+                case "blue":
+                    return Color.Blue;
+                    break;
+                default:
+                    return Color.White;
+                    break;
+            }
+            return Color.White;
+        }
+
+        public Shape(int startX, int startY, int lineWidth, string color)
+        { 
+            StartX = startX;
+            StartY = startY;
+            LineWidth = lineWidth;
+            this.color = ColorSelection(color);
+        }
+
+        public virtual double GetArea() => 0;
+        public virtual double GetPerimetr() => 0;
+        public virtual void Draw() { }
+        public virtual void Info() 
+        {
+            Console.WriteLine($"Area of the figure: {GetArea()}");
+            Console.WriteLine($"Figure perimeter:\t {GetPerimetr()}");
+            Draw();
+        }
+
 
     }
 }
